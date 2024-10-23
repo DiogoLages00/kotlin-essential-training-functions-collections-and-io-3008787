4e10d2e4c8e4ef087762935e9646dc89c3b770b8
DiogoLages00 <d.lages@campus.fct.unl.pt>
2024-10-23 18:07:15 +0100

    Completed class 03_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,8 +1,27 @@
+// Function parameters
 
 private fun printGreeting() = println("Hello Kotlin")
 
 fun main() {
-    fun getGreeting() = "Hello Kotlin"
 
-    println(getGreeting())
+    fun getGreeting(thingToGreet: String) = "Hello $thingToGreet"
+    fun getGreeting2(greeting: String, thingToGreet: String) = "$greeting $thingToGreet"
+    fun getGreetingDefaultThing(greeting: String, thingToGreet: String = "World") = "$greeting $thingToGreet"
+
+    fun getGreetingTotalDefault(
+        greeting: String = "Hello",
+        thingToGreet: String = "World"
+    ) = "$greeting $thingToGreet"
+
+    println(getGreeting("Kotlin Yeah"))
+    println(getGreeting2("Heyah", "Kotlin!"))
+
+    println(getGreetingDefaultThing("Heyah"))
+    println(getGreetingDefaultThing("Heyah", "Kotlin World"))
+    println(getGreetingTotalDefault())
+    println(getGreetingTotalDefault("Hey"))
+
+    println(getGreetingTotalDefault(thingToGreet = "World of Kotlin"))
+    println(getGreetingTotalDefault(thingToGreet = "World of Kotlin", greeting = "Hey There"))     // change order of parameters
+
 }
