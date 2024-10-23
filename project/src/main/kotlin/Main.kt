9b5c38d1f8be3e61a4b809f25dcd6982499a1748
DiogoLages00 <d.lages@campus.fct.unl.pt>
2024-10-23 22:23:44 +0100

    Completed class 04_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
+// Statements vs Expressions
+// Statement doesn't return any value
+// Expression modifies or returns value
+
+
+fun getMessage(input: Int) = if (input > 3) {
+        "The value was greater than 3"
+    }
+    else {
+        "The value was not greater than 3"
+    }
+
+fun getMessageWithWhen(input: Int) = when (input) {
+    3 -> "The value is 3"
+    else -> "The value is not 3"
+}
+
 
 
 fun main() {
-    
+    var someVariable = 5
+
+    var message = if (someVariable > 3) {
+        "The value was greater than 3"
+    }
+    else {
+        "The value was not greater than 3"
+    }
+    println(message)
+    println(getMessage(someVariable))
+
+    message = when(someVariable) {
+        3 -> "The value is 3"
+        else -> "The value is not 3"
+    }
+    println(message)
+    println(getMessageWithWhen(someVariable))
+
 }
