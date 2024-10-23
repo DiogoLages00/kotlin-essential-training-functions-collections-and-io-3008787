5d03fcc7dbdb1f53dd56962002c2c5dd06ca33fd
DiogoLages00 <d.lages@campus.fct.unl.pt>
2024-10-23 18:32:51 +0100

    Completed class 03_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
+// Higher-order functions
 
+fun printCalculatedValue(value1: Int, value2: Int, calculator: (Int, Int) -> Int) {
+    println("The value is: ${calculator(value1, value2)}")
+}
 
 fun main() {
-    
+    printCalculatedValue(2, 2, { value1, value2 ->
+        value1 + value2
+    })
+
+    printCalculatedValue(2, 2) { value1, value2 ->              // Trailing Lambda syntax
+        value1 - value2
+    }
 }
