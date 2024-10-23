b8cc119464227b816d1118859dcd97d8ca6daaf9
DiogoLages00 <d.lages@campus.fct.unl.pt>
2024-10-23 17:55:30 +0100

    Completed class 03_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,23 @@
+// Basic functions
 
-fun main() {
+fun printGreeting() {
     println("Hello World")
+}
+
+public fun getGreeting(): String {
+    return "Hello Kotlin"
+}
+
+private fun printGreeting2() = println("Hello World 2")    // Single expression function
+
+// fun getGreetingError() = println(getGreeting2())    // Cannot reach getGreeting2 (local)
+
+fun main(): Unit {        // Unit - non-meaningful return type
+    fun getGreeting2() = "Hello Kotlin 2"    // Single expression function
+
+    printGreeting()
+    println(getGreeting())
+
+    printGreeting2()
+    println(getGreeting2())   // only main can call this function (getGreeting2())
 }
