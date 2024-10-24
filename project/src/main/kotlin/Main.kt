15a224045f2e940edf57df152f1041cecebd7f48
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-24 11:49:52 +0100

    Completed class 04_06

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
+// for loops
+
 
 fun main() {
-    
+
+    for (i in 0 until 5) println(i)             // until - Exclude last number
+
+    for (i in 0..5) println(i)                  // .. - Include last number
+
+    for (i in 10 downTo 5) println(i)           // downTo - Include last number
+
+    for (i in 10 downTo 0 step 3) println(i)           // step - Jump 3 values each time
+
 }
