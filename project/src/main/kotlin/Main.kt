cd84f70851e0b091e10f50920fc70fb8652b700d
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-24 12:01:49 +0100

    Completed challenge 04_07

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -10,10 +10,33 @@ else if (input is String) { "Input was a String with length ${input.length}" }
 else if (input == null) { "Input was null" }
 else { "Input didn't match target inputs" }
 
+
+fun getOutput2(input: Any?): String = when (input) {
+    is Number -> {
+        if (input !is Int) {
+            "Input was an Int"
+        } else {
+            "Input was a non-Int Number"
+        }
+    }
+    is String -> "Input was a String with length ${input.length}"
+    null -> "Input was null"
+    else -> "Input didn't match target inputs"
+}
+
+
 fun main() {
     println(getOutput(null))
     println(getOutput(4))
     println(getOutput(3.2))
     println(getOutput("Hello Kotlin"))
     println(getOutput('a'))
+
+    println()
+
+    println(getOutput2(null))
+    println(getOutput2(4))
+    println(getOutput2(3.2))
+    println(getOutput2("Hello Kotlin"))
+    println(getOutput2('a'))
 }
