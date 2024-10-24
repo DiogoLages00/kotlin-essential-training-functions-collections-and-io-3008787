a72ce8cdf54a2ca6b71a532be8eacd5104129837
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-24 11:43:56 +0100

    Completed class 04_05

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,19 @@
+// While loops
 
 fun main() {
+
+    var counter = 0
+
+    while (counter < 5) {
+        println(counter)
+        counter++
+
+        if (counter ==3) break
+    }
+
+    do {
+        println("Do while $counter")
+        counter += 1
+    }   while (counter < 10)
 
 }
