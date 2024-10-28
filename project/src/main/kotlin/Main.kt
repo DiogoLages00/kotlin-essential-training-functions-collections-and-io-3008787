3629f45a849fed79b6a6d06688c8b6ec9b8aac0b
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 16:01:54 +0000

    Completed class 07_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import java.io.File
+
+// Challenge: Parsing a list of data from a file
+
 
 // Prompt the user to enter the name of the test scores file
 // Read each line from the file
@@ -6,5 +10,20 @@
 // Write those 3 userId/score pairs to a new file sorted.txt
 
 fun main() {
+
+    print("Enter the name of the file: ")
+    val filename = readLine() ?: ""
+
+    val file = File(filename)
+    if (!file.isFile) {
+        println("Invalid file")
+        return
+    }
+
+    val sortedFile = file.readLines()
+        .map { it -> it.split(":")[1] }
+        .sortedDescending()
+        .take(3)
+        .forEach { println(it) }
 
 }
