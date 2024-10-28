9b6e03eca43da4f42254f9337402ec2f65563dda
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 10:28:02 +0000

    Completed Challenge 06_06 - Filtering a list of data

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
+// Challenge: Filtering a list of data
+
 
 fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
 
-    
+    val lowestScores = testScores.entries
+        .sortedBy { it.value }
+        .take(3)
+        .forEach { println(it.key) }
+
 }
