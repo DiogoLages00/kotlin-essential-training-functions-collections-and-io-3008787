246a6bc1f7c880e84ec2e04fbda087b6847fb0b9
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 10:38:52 +0000

    Completed class 07_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
+// Command-line arguments
 
-fun main() {
-    
+
+fun main(vararg args: String) {
+
+    if (args.isEmpty()) {
+        println("Please pass at least one argument")
+        return
+    }
+
+    // Configuration changes are needed so that args[0] doesn't throw an ArrayIndexOutOfBounds
+    println("First arg is ${args[0]}")
+
+    for (arg in args) println(arg)
+
 }
