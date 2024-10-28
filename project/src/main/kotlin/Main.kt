40dcc73b7c7baa262be803529d810a1a1303e75d
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 19:33:56 +0000

    Completed class 08_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
+import kotlin.random.Random
+
 // Writing JUnit tests
 
 
+fun getRandom(max: Int) = Random.nextInt(max)
+
+
 fun main() {
+
+
 
 }
---
+++ project/src/test/kotlin/RandomNumberGeneratorTest.kt
@@ -0,0 +1,29 @@
+import org.junit.Test
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    fun verifyTrueIsAlwaysTrue() {
+        assert(true == true)
+    }
+
+//    @Test
+//    fun verifyTrueIsNotAlwaysTrue() {
+//        assert(true == false)
+//    }
+
+    @Test
+    fun `getRandom returns value between 0 and 100`() {
+        val random = getRandom(100)
+        assert(random in 0 .. 100)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `getRandom throws exception when negative value is passed`() {
+        getRandom(-1)
+
+    }
+
+
+
+}
