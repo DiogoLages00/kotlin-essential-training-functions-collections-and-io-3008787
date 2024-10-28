460813dc15468faa17ac1cf7774ad468a8949c83
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 10:13:37 +0000

    Completed class 06_05

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,27 @@
+// Sequences
+
 
 fun main() {
-    
+
+    val languages = listOf("kotlin", "c++", "java")
+        .filter { it.length > 4 }
+        .map { it.length }
+        // .forEach { println(it) }
+
+    // Sequences are iterable containers - apply all processes individually to each item in the collection
+    val languageSequence = sequenceOf("kotlin", "c++", "java")
+    val fromIterable = listOf(1, 2, 3).asSequence()
+
+    // Sequences are depth first iterables
+    languageSequence.filter { it.length > 4 }
+        .map { it.length }
+        .take(2)
+        .forEach { println(it) }
+
+    // Iterable vs Sequences
+    // Smaller collections (10s or 100s) - Iterable
+    // Larger collections (1000s) - Sequences
+
+
+
 }
