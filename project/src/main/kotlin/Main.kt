3b5b1ba374e7843b93c3b11a150030262f409828
Diogo Lages <CTW03934@criticaltechworks.com>
2024-10-28 11:46:08 +0000

    Completed class 07_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
+import java.io.File
+import kotlin.io.path.writeLines
+
+// Read and write files
+
 
 fun main() {
+
+    val scoresFile = File("scores.txt")
+    scoresFile.forEachLine { line -> println(line) }
+
+    val sortedLines = scoresFile.readLines().sorted()
+    val outputFile = File("sorted-scores.txt").toPath()
+    outputFile.writeLines(sortedLines)
 
 }
